7a21147c197c10ce200541463b7212800bf39d3d
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-11 23:22:43 +0530

    MOOC Part 6: Spell checker - validate words against dictionary set

--- part6/part06-07_spellchecker/src/spellchecker.py
+++ part6/part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,20 @@
-# write your solution here
+inpt_l = input("Write text: ")
+inpt = inpt_l.split()
+
+wordlst = set()
+with open('wordlist.txt') as file:
+    for line in file:
+        wordlst.add(line.strip().lower())
+
+output = []
+for word in inpt:
+    if word.lower() in wordlst:
+        output.append(word)
+    else:
+        output.append(f"*{word}*")
+
+i = 0
+while i < len(output) - 1:
+    print(output[i], end = " ")
+    i += 1
+print(output[-1])
