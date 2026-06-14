9961c29fddfff3e9526521d844ef82bbb2e133a5
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-14 20:37:26 +0530

    MOOC Part 6: Diary exercise - direct file read/write with immediate persistence

--- part6/part06-11_diary/src/diary.py
+++ part6/part06-11_diary/src/diary.py
@@ -1,1 +1,14 @@
-# Write your solution here
+while True:
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+    choice = int(input("Function: "))
+    if choice == 0:
+        print("Bye now!")
+        break
+    elif choice == 1:
+        with open("diary.txt", "a") as f:
+            f.write(input("Diary entry: ") + "\n")
+            print("Diary saved\n")
+    elif choice == 2:
+        print("Entries:")
+        with open("diary.txt", "r") as f:
+            print(f.read())
