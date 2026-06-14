4168b92efab6e2f1e233a7e658f5edca9a410615
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-14 13:07:45 +0530

    MOOC Part 6: CityBikes - station data parsing, distance calc, greatest pair [LESSONS REAPPLIED]

--- part6/part06-09_city_bikes/src/city_bikes.py
+++ part6/part06-09_city_bikes/src/city_bikes.py
@@ -1,54 +1,42 @@
 import math
 def get_station_data(filename: str):
-    station = {}
-
+    stations_names = {}
     with open(filename) as file:
         for line in file:
-            line = line.split(';')
+            line = line.strip().split(';')
             if line[0] == 'Longitude':
                 continue
-            station[line[3]] = (float(line[0]), float(line[1]))
-    
-    return station
+            stations_names[line[3]] = (float(line[0]), float(line[1]))
+    return stations_names
+
+# st = {}
+# line = "24.950292890004903;60.155444793742276;1;Kaivopuisto;30;Yes;1".strip().split(';')
+# print(line)
+# st[line[3]] = (float(line[0]), float(line[1]))
+# print(st)
+# print(list(st))
 
 def distance(stations: dict, station1: str, station2: str):
-    
-    longitude1 = float(stations[station1][0])
-    longitude2 = float(stations[station2][0])
-    latitude1 = float(stations[station1][1])
-    latitude2 = float(stations[station2][1])
+    longitude1, longitude2 = stations[station1][0], stations[station2][0]
+    latitude1, latitude2 = stations[station2][1], stations[station1][1]
+
     x_km = (longitude1 - longitude2) * 55.26
     y_km = (latitude1 - latitude2) * 111.2
     distance_km = math.sqrt(x_km**2 + y_km**2)
     return distance_km
 
-# def greatest_distance(stations: dict):
-#     fst = stations[0][0]
-#     scnd = stations[1][0]
-#     greatest_dis = distance(stations: stations[0], stations[1])
-#     keys = list(stations.keys())
-#     for first in range(len(keys)):
-#         for second in range(first + 1, len(keys)):
-#             if distance(stations, stations[first], stations[second]) > greatest_dis:
-#                 greatest_dist = distance(stations, stations[first], stations[second])
-#                 fst = staions[first]
-#                 scnd = stations[second]
-#     return f"{fst} {scnd} {greatest_dis}"
-
-# used help in the below function to keep the streak alive.
-# will recommit once the streak is alive and will solve it myself
 def greatest_distance(stations: dict):
-    keys = list(stations.keys())
-    fst = keys[0]
-    scnd = keys[1]
-    greatest_dis = distance(stations, keys[0], keys[1])
+    stations_name = list(stations)
+    stations_without_1st = {name: stations[name] for name in stations_name[1:]}
+
+    greatest_dis = 0.0
+    st1 = ""
+    st2 = ""
+    for i in stations:
+        for j in stations_without_1st:
+            if distance(stations, i, j) > greatest_dis:
+                greatest_dis = distance(stations, i, j)
+                st1 = i
+                st2 = j
     
-    for i in range(len(keys)):
-        for j in range(i + 1, len(keys)):
-            d = distance(stations, keys[i], keys[j])
-            if d > greatest_dis:
-                greatest_dis = d
-                fst = keys[i]
-                scnd = keys[j]
-    
-    return (fst, scnd, greatest_dis)
+    return st1, st2, greatest_dis
