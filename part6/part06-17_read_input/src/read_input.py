49f6b460bdeffbb0cdcb6659ffda904c9849fa46
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-19 10:36:27 +0530

    MOOC Part 6: Reading input - exception handling with range validation

--- part6/part06-17_read_input/src/read_input.py
+++ part6/part06-17_read_input/src/read_input.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def read_input(st: str, lower_limit: int, upper_limit: int):
+    while True:
+        try:
+            num = int(input(st))
+            if lower_limit < num < upper_limit:
+                return num
+        except ValueError:
+            pass
+        print(f'You must type in an integer between {lower_limit} and {upper_limit}')
+
+# number = read_input("Please type in a number: ", 1, 5)
+# print("You typed in:", number)
