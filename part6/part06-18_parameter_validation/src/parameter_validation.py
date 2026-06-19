67de2469171575cec87f03cb364f2493620110d7
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-19 11:22:51 +0530

    MOOC Part 6: Parameter validation - new_person tuple creation with multi-word check

--- part6/part06-18_parameter_validation/src/parameter_validation.py
+++ part6/part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,6 @@
-# Write your solution here
+def new_person(name: str, age: int):
+    if len(name.split()) < 2 or len(name) > 40 or age < 0 or age > 150:
+        raise ValueError
+    return name, age
+
+# print(new_person('Andrew', 32))
