c8be36e7ac391f058b9f7543cadc55b42cecadf2
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-20 18:53:28 +0530

    MOOC Part 7: Lottery numbers - using random.sample for unique selection

--- part7/part07-04_lottery_numbers/src/lottery_numbers.py
+++ part7/part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,7 @@
-# Write your solution here
+from random import sample
+
+
+def lottery_numbers(amount: int, lower: int, upper: int):
+    num = list(range(lower, upper + 1))
+    l = sample(num, amount)
+    return sorted(l)
