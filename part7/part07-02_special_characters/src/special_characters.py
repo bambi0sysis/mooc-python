1cf41c1700804719c653b6572d9bbb5e642622b6
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-20 12:59:37 +0530

    MOOC Part 7: Separate characters - single-pass logic with string module

--- part7/part07-02_special_characters/src/special_characters.py
+++ part7/part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,13 @@
-# Write your solution here
+import string
+
+
+def separate_characters(my_string: str):
+    str1 = str2 = str3 = ""
+    for char in my_string:
+        if char in string.ascii_letters:
+            str1 += char
+        elif char in string.punctuation:
+            str2 += char
+        else:
+            str3 += char
+    return str1, str2, str3
