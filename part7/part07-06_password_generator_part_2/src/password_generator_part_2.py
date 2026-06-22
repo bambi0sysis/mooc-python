12d3ba7d875b1ab770d9dc2fb410b4aa1408296a
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-22 18:27:10 +0530

    MOOC Part 7: Strong password generator - list extension and shuffling

--- part7/part07-06_password_generator_part_2/src/password_generator_part_2.py
+++ part7/part07-06_password_generator_part_2/src/password_generator_part_2.py
@@ -1,1 +1,28 @@
-# Write your solution here
+from random import choice, shuffle, randint
+from string import ascii_lowercase
+
+
+def generate_strong_password(length: int, arg2: bool, arg3: bool):
+    if length >= 3:
+        num_of_nums = randint(1, length - 2)
+    # print(num_of_nums)
+    psswrd = []
+
+    if arg2:
+        nums = [str(n) for n in range(0, 10)]
+        shuffle(nums)
+        psswrd.extend(nums[:num_of_nums])
+
+    if arg3:
+        symbols = list("!?=+-()#")
+        shuffle(symbols)
+        psswrd.extend(symbols[: length - num_of_nums - 1])
+
+    for i in range(length - len(psswrd)):
+        psswrd.append(choice(ascii_lowercase))
+
+    return "".join(psswrd)
+
+
+# for i in range(10):
+#     print(generate_strong_password(2, False, False))
