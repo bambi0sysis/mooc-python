7b36e4583502ec8f6b80252ea5032d8f29198abd
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-23 16:26:49 +0530

    MOOC Part 7: Random words - file filtering and sampling with error handling

--- part7/part07-08_random_words/src/random_words.py
+++ part7/part07-08_random_words/src/random_words.py
@@ -1,1 +1,12 @@
-# Write your solution here
+from random import sample
+
+def words(n: int, beginning: str):
+    result = []
+    with open('words.txt') as file:
+        for row in file:
+            row = row.strip()
+            if row.startswith(beginning) and row not in result:
+                result.append(row)
+    if len(result) < n:
+        raise ValueError('Not enough words beginning with the specified string!')
+    return sample(result, n)
