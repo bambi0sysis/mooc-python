59d28d2b9584813bd83ab4e465aad2e4712ec3ae
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-24 22:02:02 +0530

    MOOC Part 7: How old - datetime calculation for millennium eve

--- part7/part07-09_how_old/src/how_old.py
+++ part7/part07-09_how_old/src/how_old.py
@@ -1,1 +1,15 @@
-# Write your solution here
+from datetime import datetime
+
+day = int(input('Data: '))
+month = int(input('Month: '))
+year = int(input('Year: '))
+
+dob = datetime(year, month, day)
+eve = datetime(1999, 12, 31)
+
+difference = eve - dob
+
+if difference.days > 0:
+    print(f'You were {difference.days} days old on the eve of the new millennium.')
+else:
+    print("You weren't born yet on the eve of the new millennium.")
