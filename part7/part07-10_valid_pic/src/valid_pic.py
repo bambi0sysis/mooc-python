705b9500b758432037bbc7511d76a335498c57db
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-24 23:33:19 +0530

    MOOC Part 7: Valid PIC - checksum validation and date parsing

--- part7/part07-10_valid_pic/src/valid_pic.py
+++ part7/part07-10_valid_pic/src/valid_pic.py
@@ -1,1 +1,24 @@
-# Write your solution here
+from datetime import datetime
+
+
+def is_it_valid(pic: str) -> bool:
+    if len(pic) != 11:
+        return False
+
+    ddmmyy = pic[:6]
+    century = pic[6]
+    century_marker = {"+": 18, "-": 19, "A": 20}
+    picNum = pic[7:10]
+    control_ch_remainder = int(ddmmyy + picNum) % 31
+    string = "0123456789ABCDEFHJKLMNPRSTUVWXY"
+
+    try:
+        if century not in century_marker:
+            raise ValueError("invalid century!")
+        datetime(
+            century_marker[century] + int(ddmmyy[4:]), int(ddmmyy[2:4]), int(ddmmyy[:2])
+        )
+        return pic[-1] == string[control_ch_remainder]
+
+    except ValueError:
+        return False
