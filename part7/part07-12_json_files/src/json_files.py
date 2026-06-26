8a5b280a3e3f329331b0c945919753560ff73306
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-26 10:52:21 +0530

    MOOC Part 7: JSON files - parsing and formatted output with join

--- part7/part07-12_json_files/src/json_files.py
+++ part7/part07-12_json_files/src/json_files.py
@@ -1,1 +1,17 @@
-# Write your solution here
+import json
+
+def print_persons(filename: str):
+
+    with open(filename) as file:
+        data = file.read()
+
+    data_in_py = json.loads(data)
+
+    for dictionary in data_in_py:
+        print(f'{dictionary['name']} {dictionary['age']} years (', end = "")
+        if dictionary['hobbies']:
+            for hobby in dictionary['hobbies'][:-1]:
+                print(f'{hobby}, ', end = "")
+            print(f'{dictionary['hobbies'][-1]})')
+        else:
+            print(')')
