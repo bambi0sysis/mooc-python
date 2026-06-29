2ecfcb5aa616e3a3af9c65dafacd26b85c33e394
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-30 00:09:22 +0530

    MOOC Part 7: String manipulation - refactored split_in_half and change_case

--- part7/part07-17_string_helper/src/string_helper.py
+++ part7/part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,20 @@
-# Write your solution here
+from string import ascii_letters, digits
+
+
+def change_case(orig_string: str):
+    result = ""
+    for letter in orig_string:
+        result += letter.lower() if letter.isupper() else letter.upper()
+    return result
+
+
+def split_in_half(orig_string: str):
+    return orig_string[: len(orig_string) // 2], orig_string[len(orig_string) // 2 :]
+
+
+def remove_special_characters(orig_string: str):
+    result = ""
+    for letter in orig_string:
+        if letter in ascii_letters or letter in digits or letter == " ":
+            result += letter
+    return result
