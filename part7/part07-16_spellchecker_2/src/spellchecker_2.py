c0fcab39fe137fadfa2b4dba7ec02170de669449
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-06-29 19:06:32 +0530

    MOOC Part 7: Spellchecker 2 - difflib integration and case-insensitive matching

--- part7/part07-16_spellchecker_2/src/spellchecker_2.py
+++ part7/part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,25 @@
-# Write your solution here
+from difflib import get_close_matches
+
+sentence = input("write text: ").split()
+result = []
+incorrect = []
+
+file_content = []
+
+with open("wordlist.txt") as file:
+    for word in file:
+        file_content.append(word.strip())
+
+for word in sentence:
+    if word.lower() in file_content:
+        result.append(word)
+        continue
+    result.append(f"*{word}*")
+    incorrect.append(word)
+
+print(" ".join(result))
+print("suggestions:")
+
+for word in incorrect:
+    output = get_close_matches(word, file_content)
+    print(f"{word}: {', '.join(output)}")
