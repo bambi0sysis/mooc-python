65f74ec055b2fb027913a03a8cf7a4a81c68015e
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-04 16:06:12 +0530

    MOOC Part 8: NumberStats with running sum optimization

--- part8/part08-12_number_stats/src/number_stats.py
+++ part8/part08-12_number_stats/src/number_stats.py
@@ -1,10 +1,40 @@
-# Write your solution here!
-class  NumberStats:
+class NumberStats:
     def __init__(self):
         self.numbers = 0
+        self.count = 0
 
-    def add_number(self, number:int):
-        pass
+    def add_number(self, number: int):
+        self.numbers += number
+        self.count += 1
 
     def count_numbers(self):
-        pass
+        return self.count
+
+    def get_sum(self):
+        return self.numbers
+
+    def average(self):
+        if self.count != 0:
+            return self.numbers / self.count
+        return 0
+
+
+num = NumberStats()
+odd_num = NumberStats()
+even_num = NumberStats()
+
+
+while True:
+    inpt = int(input("Please type in integer numbers: "))
+    if inpt == -1:
+        break
+    num.add_number(inpt)
+    if inpt % 2 == 0:
+        even_num.add_number(inpt)
+    else:
+        odd_num.add_number(inpt)
+
+print("Sum of numbers:", num.get_sum())
+print("Mean of numbers:", num.average())
+print("Sum of even numbers:", even_num.get_sum())
+print("Sum of odd numbers:", odd_num.get_sum())
