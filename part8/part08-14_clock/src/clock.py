668afafb329eaa8d997703236a6a946b299f4fcb
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-05 12:50:25 +0530

    MOOC Part 8: Clock class with tick, set, and formatting

--- part8/part08-14_clock/src/clock.py
+++ part8/part08-14_clock/src/clock.py
@@ -1,1 +1,24 @@
-# Write your solution here:
+class Clock:
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def tick(self):
+        self.seconds += 1
+        if self.seconds == 60:
+            self.seconds = 0
+            self.minutes += 1
+        if self.minutes == 60:
+            self.minutes = 0
+            self.hours += 1
+        if self.hours == 24:
+            self.hours = self.minutes = self.seconds = 0
+
+    def set(self, hour: int, minute: int):
+        self.hours = hour
+        self.minutes = minute
+        self.seconds = 0
+
+    def __str__(self):
+        return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
