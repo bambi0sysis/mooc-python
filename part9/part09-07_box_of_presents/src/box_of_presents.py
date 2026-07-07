9e1293166d9a9e2d012d0a92b713734ffd8c579a
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-07 14:20:48 +0530

    MOOC Part 9: Box class with composition and aggregation

--- part9/part09-07_box_of_presents/src/box_of_presents.py
+++ part9/part09-07_box_of_presents/src/box_of_presents.py
@@ -1,1 +1,21 @@
-# WRITE YOUR SOLUTION HERE:
+class Present:
+    def __init__(self, name: str, weight: int):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.name} ({self.weight} kg)"
+
+
+class Box:
+    def __init__(self):
+        self.box = []
+
+    def add_present(self, present: Present):
+        self.box.append(present)
+
+    def total_weight(self):
+        combined_weight = 0
+        for gift in self.box:
+            combined_weight += gift.weight
+        return combined_weight
