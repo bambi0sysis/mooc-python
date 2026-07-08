fc2adca32d6bed74574db06a1d19af49391c22f2
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-08 15:57:33 +0530

    MOOC Part 9: WeatherStation with list-based observation tracking

--- part9/part09-11_weather_station/src/weather_station.py
+++ part9/part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,18 @@
-# WRITE YOUR SOLUTION HERE:
+class WeatherStation:
+    def __init__(self, name: str):
+        self.__name = name
+        self.__observations = []
 
+    def add_observation(self, observation: str):
+        self.__observations.append(observation)
 
+    def latest_observation(self):
+        if self.__observations:
+            return self.__observations[-1]
+        return ""
+
+    def number_of_observations(self):
+        return len(self.__observations)
+
+    def __str__(self):
+        return f"{self.__name}, {len(self.__observations)} observations"
