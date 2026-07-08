cc426bdaadf0344486234290263d2104d9055ae4
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-08 15:40:47 +0530

    MOOC Part 9: Recording class with property decorators and validation

--- part9/part09-10_recording/src/recording.py
+++ part9/part09-10_recording/src/recording.py
@@ -1,1 +1,17 @@
-# WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self, length: int):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("The length should not be below zero")
+
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, length_changed: int):
+        if length_changed >= 0:
+            self.__length = length_changed
+        else:
+            raise ValueError("The length should not be below zero")
