d4b2445a8a2488087478452b941e16640e3cf217
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-08 15:23:56 +0530

    MOOC Part 9: Car class with encapsulation and fuel logic

--- part9/part09-09_car/src/car.py
+++ part9/part09-09_car/src/car.py
@@ -1,1 +1,18 @@
-# WRITE YOUR SOLUTION HERE:
+class Car:
+    def __init__(self):
+        self.__petrol_amount = 0
+        self.__reading = 0
+
+    def fill_up(self):
+        self.__petrol_amount = 60
+
+    def drive(self, km: int):
+        if self.__petrol_amount - km >= 0:
+            self.__petrol_amount -= km
+            self.__reading += km
+        else:
+            self.__reading += self.__petrol_amount
+            self.__petrol_amount = 0
+
+    def __str__(self):
+        return f"Car: odometer reading {self.__reading} km, petrol remaining {self.__petrol_amount} litres"
