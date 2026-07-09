b211309aec14cb8e6361129c539981825c476f49
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-09 19:08:15 +0530

    MOOC Part 9: ListHelper with classmethods and frequency logic

--- part9/part09-14_list_helper/src/list_helper.py
+++ part9/part09-14_list_helper/src/list_helper.py
@@ -1,1 +1,23 @@
-# WRITE YOUR SOLUTION HERE:
+class ListHelper:
+    def __init__(self, lst: list):
+        self.lst = lst
+
+    @classmethod
+    def greatest_frequency(cls, my_list: list):
+        if my_list:
+            most_common = my_list[0]
+        highest_count = 0
+        for item in my_list:
+            if my_list.count(item) > highest_count:
+                most_common = item
+                highest_count = my_list.count(item)
+        return most_common
+
+    @classmethod
+    def doubles(cls, my_list: list):
+        doubles_my_list = []
+        for item in my_list:
+            if my_list.count(item) > 1:
+                if item not in doubles_my_list:
+                    doubles_my_list.append(item)
+        return len(doubles_my_list)
