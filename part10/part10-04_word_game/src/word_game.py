790daadc5ca19e826028f13a8ac5b917451a3bd0
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-11 19:16:23 +0530

    MOOC Part 9: WordGame inheritance with RPS and vowel counting

--- part10/part10-04_word_game/src/word_game.py
+++ part10/part10-04_word_game/src/word_game.py
@@ -1,7 +1,7 @@
-# Write your solution here
 import random
 
-class WordGame():
+
+class WordGame:
     def __init__(self, rounds: int):
         self.wins1 = 0
         self.wins2 = 0
@@ -13,7 +13,7 @@ class WordGame():
 
     def play(self):
         print("Word game:")
-        for i in range(1, self.rounds+1):
+        for i in range(1, self.rounds + 1):
             print(f"round {i}")
             answer1 = input("player1: ")
             answer2 = input("player2: ")
@@ -25,9 +25,68 @@ class WordGame():
                 self.wins2 += 1
                 print("player 2 won")
             else:
-                pass # it's a tie
+                pass  # it's a tie
 
         print("game over, wins:")
         print(f"player 1: {self.wins1}")
         print(f"player 2: {self.wins2}")
 
+
+class LongestWord(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        if len(player1_word) > len(player2_word):
+            return 1
+        elif len(player2_word) > len(player1_word):
+            return 2
+        return 0
+
+
+class MostVowels(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        vowels = "aeiou"
+        p1 = p2 = 0
+        for letter in player1_word:
+            if letter in vowels:
+                p1 += 1
+        for letter in player2_word:
+            if letter in vowels:
+                p2 += 1
+
+        if p1 > p2:
+            return 1
+        elif p2 > p1:
+            return 2
+        return 0
+
+
+class RockPaperScissors(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        if player1_word == player2_word:
+            return 0
+        if player1_word not in ["rock", "paper", "scissors"] and player2_word not in [
+            "rock",
+            "paper",
+            "scissors",
+        ]:
+            return 0
+        if player1_word not in ["rock", "paper", "scissors"]:
+            return 2
+        if player2_word not in ["rock", "paper", "scissors"]:
+            return 1
+        if player1_word == "rock" and player2_word == "scissors":
+            return 1
+        elif player1_word == "paper" and player2_word == "rock":
+            return 1
+        elif player1_word == "scissors" and player2_word == "paper":
+            return 1
+        else:
+            return 2
