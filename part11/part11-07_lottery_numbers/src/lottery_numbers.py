6ae4a98d5b0e635f742168d6e744b0ce436c4e13
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-14 18:26:21 +0530

    MOOC Part 10: LotteryNumbers with encapsulated winning numbers and conditional comprehension

--- part11/part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11/part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,10 @@
-# WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, week_no: int, lottery_numbers: list):
+        self.__week_no = week_no
+        self.__lottery_numbers = lottery_numbers
+
+    def number_of_hits(self, numbers: list):
+        return len([num for num in numbers if num in self.__lottery_numbers])
+
+    def hits_in_place(self, numbers: list):
+        return [num if num in self.__lottery_numbers else -1 for num in numbers]
