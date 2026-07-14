cb30a9ba2d8e297fd4a1351a25fc9278efefee22
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-14 23:21:02 +0530

    MOOC Part 10: most_common_words with case-sensitive frequency counting

--- part11/part11-12_most_common_words/src/most_common_words.py
+++ part11/part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,14 @@
-# WRITE YOUR SOLUTION HERE:
+from string import punctuation
+
+
+def most_common_words(filename: str, lower_limit: int):
+    freq = {}
+    with open(filename) as file:
+        for line in file:
+            line = line.replace("\n", " ")
+            for char in punctuation:
+                line = line.replace(char, "")
+            for word in line.split():
+                if word:
+                    freq[word] = freq.get(word, 0) + 1
+    return {word: count for word, count in freq.items() if count >= lower_limit}
