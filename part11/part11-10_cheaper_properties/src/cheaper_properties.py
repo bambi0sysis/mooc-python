5e5a9895d9786fffe4020dedc83be5fd573df624
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-14 22:31:16 +0530

    MOOC Part 10: cheaper_properties with concise list comprehension

--- part11/part11-10_cheaper_properties/src/cheaper_properties.py
+++ part11/part11-10_cheaper_properties/src/cheaper_properties.py
@@ -1,5 +1,7 @@
 class RealProperty:
-    def __init__(self, rooms: int , square_meters: int, price_per_sqm: int, description: str):
+    def __init__(
+        self, rooms: int, square_meters: int, price_per_sqm: int, description: str
+    ):
         self.rooms = rooms
         self.square_meters = square_meters
         self.price_per_sqm = price_per_sqm
@@ -10,16 +12,24 @@ class RealProperty:
 
     def price_difference(self, compared_to):
         # Function abs returns absolute value
-        difference = abs((self.price_per_sqm * self.square_meters) - (compared_to.price_per_sqm * compared_to.square_meters))
+        difference = abs(
+            (self.price_per_sqm * self.square_meters)
+            - (compared_to.price_per_sqm * compared_to.square_meters)
+        )
         return difference
 
     def more_expensive(self, compared_to):
-        difference = (self.price_per_sqm * self.square_meters) - (compared_to.price_per_sqm * compared_to.square_meters)
+        difference = (self.price_per_sqm * self.square_meters) - (
+            compared_to.price_per_sqm * compared_to.square_meters
+        )
         return difference > 0
 
-
     def __repr__(self):
-        return (f'RealProperty(rooms = {self.rooms}, square_meters = {self.square_meters}, ' + 
-            f'price_per_sqm = {self.price_per_sqm}, description = {self.description})')
+        return (
+            f"RealProperty(rooms = {self.rooms}, square_meters = {self.square_meters}, "
+            + f"price_per_sqm = {self.price_per_sqm}, description = {self.description})"
+        )
 
-# WRITE YOUR SOLUTION HERE:
+
+def cheaper_properties(properties: list, ref: RealProperty):
+    return [(p, ref.price_difference(p)) for p in properties if ref.more_expensive(p)]
