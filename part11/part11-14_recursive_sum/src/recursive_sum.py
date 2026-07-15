e4a50cff8afcd20212f7d1e63c51d38c4cf788aa
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-15 16:24:27 +0530

    better version of previous problem

--- part11/part11-14_recursive_sum/src/recursive_sum.py
+++ part11/part11-14_recursive_sum/src/recursive_sum.py
@@ -1,6 +1,4 @@
 def recursive_sum(number: int):
-    summ = number
     if number <= 1:
         return number
-    summ += recursive_sum(number - 1)
-    return summ
+    return number + recursive_sum(number - 1)
