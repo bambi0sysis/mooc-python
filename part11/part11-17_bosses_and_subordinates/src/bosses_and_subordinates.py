61ae0487b1e1bd643aab6cdd8568260ec8b67056
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-16 22:58:22 +0530

    MOOC Part 11: count_subordinates with recursive tree traversal

--- part11/part11-17_bosses_and_subordinates/src/bosses_and_subordinates.py
+++ part11/part11-17_bosses_and_subordinates/src/bosses_and_subordinates.py
@@ -1,8 +1,16 @@
-# WRITE YOUR SOLUTION HERE:
 class Employee:
     def __init__(self, name: str):
         self.name = name
         self.subordinates = []
 
-    def add_subordinate(self, employee: 'Employee'):
+    def add_subordinate(self, employee: "Employee"):
         self.subordinates.append(employee)
+
+
+def count_subordinates(employee: Employee):
+    if len(employee.subordinates) == 0:
+        return 0
+    count = len(employee.subordinates)
+    for member in employee.subordinates:
+        count += count_subordinates(member)
+    return count
