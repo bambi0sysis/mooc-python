ee171d5e756a55833ba264b802c3e0015f53bbe7
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-17 21:23:22 +0530

    MOOC Part 12: BallPlayer stats with max/min key functions

--- part12/part12-06_ballplayers/src/ballplayers.py
+++ part12/part12-06_ballplayers/src/ballplayers.py
@@ -7,8 +7,27 @@ class BallPlayer:
         self.minutes = minutes
 
     def __str__(self):
-        return (f'BallPlayer(name={self.name}, number={self.number}, '
-            f'goals={self.goals}, passes={self.passes}, minutes={self.minutes})')
+        return (
+            f"BallPlayer(name={self.name}, number={self.number}, "
+            f"goals={self.goals}, passes={self.passes}, minutes={self.minutes})"
+        )
 
 
-# Write your solution here
+def most_goals(players: list):
+    most_goals_by = [(player.name, player.goals) for player in players]
+    most_goals_by.sort(key=lambda t: t[1])
+    return most_goals_by[-1][0]
+
+
+def most_points(players: list):
+    players_data = [
+        (player.name, player.number, player.goals + player.passes) for player in players
+    ]
+    players_data.sort(key=lambda t: t[-1])
+    return players_data[-1][0], players_data[-1][1]
+
+
+def least_minutes(players: list):
+    least_min = [(player, player.minutes) for player in players]
+    least_min.sort(key=lambda t: t[-1])
+    return least_min[0][0]
