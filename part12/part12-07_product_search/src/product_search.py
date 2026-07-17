a4795cb7dc6b0a8b66fabca4b1b200a127773db5
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-17 21:40:32 +0530

    MOOC Part 12: search with callable criterion

--- part12/part12-07_product_search/src/product_search.py
+++ part12/part12-07_product_search/src/product_search.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def search(products: list, criterion: callable):
+    eligible_products = []
+    for product in products:
+        if criterion(product):
+            eligible_products.append(product)
+    return eligible_products
+
+
+def price_under_4_euros(product):
+    return product[1] < 4
