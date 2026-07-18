62ac31d7ed75ce0a294cba89f934d10dd1917cc7
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-18 20:33:00 +0530

    MOOC Part 12: prime_numbers generator with for-else optimization

--- part12/part12-09_prime_numbers/src/prime_numbers.py
+++ part12/part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,9 @@
-# Write your solution here
+def prime_numbers():
+    n = 2
+    while True:
+        for i in range(2, n):
+            if n % i == 0:
+                break
+        else:
+            yield n
+        n += 1
