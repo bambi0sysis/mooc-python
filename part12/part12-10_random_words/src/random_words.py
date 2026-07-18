92e8b4cac4fe7e6393db18737da76f56b9de46c1
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-18 20:52:12 +0530

    MOOC Part 12: word_generator with random sampling

--- part12/part12-10_random_words/src/random_words.py
+++ part12/part12-10_random_words/src/random_words.py
@@ -1,1 +1,11 @@
-# Write your solution here:
+from random import sample
+
+
+# def word_generator(characters: str, length: int, amount: int):
+#     for _ in range(amount):
+#         yield "".join(sample(characters, length))
+
+
+def word_generator(characters: str, length: int, amount: int):
+    gen = ("".join(sample(characters, length)) for _ in range(amount))
+    return gen
