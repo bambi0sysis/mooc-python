075d88849cb3429e2b15ecec579848bdac0de3f1
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-19 22:02:59 +0530

    MOOC Part 12: CourseAttempt with map and set deduplication

--- part12/part12-10_random_words/src/random_words.py
+++ part12/part12-10_random_words/src/random_words.py
@@ -1,11 +1,26 @@
-from random import sample
-
-
-# def word_generator(characters: str, length: int, amount: int):
-#     for _ in range(amount):
-#         yield "".join(sample(characters, length))
+from random import choice
 
 
 def word_generator(characters: str, length: int, amount: int):
-    gen = ("".join(sample(characters, length)) for _ in range(amount))
-    return gen
+    for _ in range(amount):
+        yield "".join(choice(characters) for _ in range(length))
+
+
+# def word_generator(characters: str, length: int, amount: int):
+#     gen = ("".join(choice(characters) for _ in range(length)) for _ in range(amount))
+#     return gen
+
+
+# 1st version
+
+# from random import sample
+
+
+# # def word_generator(characters: str, length: int, amount: int):
+# #     for _ in range(amount):
+# #         yield "".join(sample(characters, length))
+
+
+# def word_generator(characters: str, length: int, amount: int):
+#     gen = ("".join(sample(characters, length)) for _ in range(amount))
+#     return gen
--- part12/part12-11_attempted_courses/src/attempted_courses.py
+++ part12/part12-11_attempted_courses/src/attempted_courses.py
@@ -5,6 +5,14 @@ class CourseAttempt:
         self.grade = grade
 
     def __str__(self):
-        return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+        return (
+            f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+        )
 
-# Write your solution here
+
+def names_of_students(attempts: list):
+    return list(map(lambda x: x.student_name, attempts))
+
+
+def course_names(attempts: list):
+    return sorted(list(set(map(lambda x: x.course_name, attempts))))
