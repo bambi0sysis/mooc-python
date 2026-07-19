8ab0b0668f303602f2acd703977dfda226274b46
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-19 23:40:52 +0530

    MOOC Part 12: CourseAttempt with reduce for aggregation

--- part12/part12-13_credits/src/credits.py
+++ part12/part12-13_credits/src/credits.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+
 class CourseAttempt:
     def __init__(self, course_name: str, grade: int, credits: int):
         self.course_name = course_name
@@ -9,4 +10,18 @@ class CourseAttempt:
     def __str__(self):
         return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
 
-# Write your solution
+
+def sum_of_all_credits(attempts: list):
+    return reduce(lambda summ, x: summ + x.credits, attempts, 0)
+
+
+def sum_of_passed_credits(attempts: list):
+    attempts_filtered = filter(lambda x: x.grade > 0, attempts)
+    return reduce(lambda summ, x: summ + x.credits, attempts_filtered, 0)
+
+
+def average(attempts: list):
+    attempts_filtered = list(filter(lambda x: x.grade > 0, attempts))
+    return reduce(lambda summ, x: summ + x.grade, attempts_filtered, 0) / len(
+        attempts_filtered
+    )
