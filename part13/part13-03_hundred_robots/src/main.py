4200a43e3d0f28e001fd6add2e0387874fe68aa0
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-26 17:41:29 +0530

    MOOC Part 13: PyGame Random Robots with optimized randint

--- part13/part13-03_hundred_robots/src/main.py
+++ part13/part13-03_hundred_robots/src/main.py
@@ -1,16 +1,16 @@
 import pygame
- 
+
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
- 
+
 robot = pygame.image.load("robot.png")
- 
+
 screen.fill((0, 0, 0))
 for i in range(10):
     for j in range(10):
-        screen.blit(robot, (50+10*i+40*j, 100+i*20))
-pygame.display.flip() 
- 
+        screen.blit(robot, (50 + 10 * i + 40 * j, 100 + i * 20))
+pygame.display.flip()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- part13/part13-04_random_robots/src/main.py
+++ part13/part13-04_random_robots/src/main.py
@@ -1,1 +1,19 @@
-# WRITE YOUR SOLUTION HERE:
+from random import randint
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+width = robot.get_width()
+height = robot.get_height()
+
+screen.fill((0, 0, 0))
+for i in range(1000):
+    screen.blit(robot, (randint(0, 640 - width), randint(0, 480 - height)))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
