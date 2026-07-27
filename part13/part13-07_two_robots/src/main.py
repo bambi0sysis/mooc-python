2904c2725d5537152f1e454484402a2bc8a918df
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-27 17:25:52 +0530

    MOOC Part 13: PyGame Dual Robot Bounce with independent velocities

--- part13/part13-07_two_robots/src/main.py
+++ part13/part13-07_two_robots/src/main.py
@@ -1,1 +1,38 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+width, height = 640, 480
+robot = pygame.image.load("robot.png")
+
+x1 = 0
+y1 = 50
+x2 = 0
+y2 = 150
+velocity = 1
+velocity2 = 2
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x1, y1))
+    window.blit(robot, (x2, y2))
+    pygame.display.flip()
+
+    x1 += velocity
+    x2 += velocity2
+    if velocity > 0 and x1 + robot.get_width() >= width:
+        velocity = -velocity
+    if velocity < 0 and x1 <= 0:
+        velocity = -velocity
+
+    if velocity2 > 0 and x2 + robot.get_width() >= width:
+        velocity2 = -velocity2
+    if velocity2 < 0 and x2 <= 0:
+        velocity2 = -velocity2
+
+    clock.tick(60)
