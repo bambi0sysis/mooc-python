294374dbfab6332af560b32c317297d597718bdd
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-07-27 17:52:39 +0530

    MOOC Part 13: PyGame Circular Robot Animation with angular spacing

--- part13/part13-08_robot_circle/src/main.py
+++ part13/part13-08_robot_circle/src/main.py
@@ -1,1 +1,29 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+import math
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+angle = 0
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    x = 320 + math.cos(angle) * 140 - robot.get_width() / 2
+    y = 240 + math.sin(angle) * 140 - robot.get_height() / 2
+
+    window.fill((0, 0, 0))
+    for i in range(10):
+        current_angle = angle + (i * (2 * math.pi / 10))
+        x = 320 + math.cos(current_angle) * 140 - robot.get_width() / 2
+        y = 240 + math.sin(current_angle) * 140 - robot.get_height() / 2
+        window.blit(robot, (x, y))
+    pygame.display.flip()
+
+    angle += 0.01
+    clock.tick(60)
