581984d91f8527a049e9c8baf87749c1e17906d0
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-08-01 17:55:10 +0530

    MOOC Part 13: Keyboard Robot with Boundary Clamping

--- part13/part13-12_four_walls/src/main.py
+++ part13/part13-12_four_walls/src/main.py
@@ -1,1 +1,58 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+width, height = 640, 480
+
+robot = pygame.image.load("robot.png")
+robot_width = robot.get_width()
+robot_height = robot.get_height()
+x = 0
+y = height - robot.get_height()
+
+to_right = False
+to_left = False
+to_up = False
+to_down = False
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_left = True
+            if event.key == pygame.K_RIGHT:
+                to_right = True
+            if event.key == pygame.K_UP:
+                to_up = True
+            if event.key == pygame.K_DOWN:
+                to_down = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                to_left = False
+            if event.key == pygame.K_RIGHT:
+                to_right = False
+            if event.key == pygame.K_UP:
+                to_up = False
+            if event.key == pygame.K_DOWN:
+                to_down = False
+
+        if event.type == pygame.QUIT:
+            exit()
+
+    if to_right and x + 2 + robot_width <= width:
+        x += 2
+    if to_left and x - 2 >= 0:
+        x -= 2
+    if to_up and y - 2 >= 0:
+        y -= 2
+    if to_down and y + 2 + robot_height <= height:
+        y += 2
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    pygame.display.flip()
+
+    clock.tick(60)
