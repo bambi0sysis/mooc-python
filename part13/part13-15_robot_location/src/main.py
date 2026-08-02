039e69060337d2e7058fdc09f824c785343ff6f4
bambi0sysis <andidotooandmaybeyoutoo@gmail.com>
2026-08-02 17:21:23 +0530

    MOOC Part 13: Click-to-Move Robot with AABB Collision

--- part13/part13-15_robot_location/src/main.py
+++ part13/part13-15_robot_location/src/main.py
@@ -1,1 +1,27 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+import random
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+width, height = 640, 480
+
+robot = pygame.image.load("robot.png")
+robot_w = robot.get_width()
+robot_h = robot.get_height()
+
+x = random.randint(0, width - robot_w)
+y = random.randint(0, height - robot_h)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x = random.randint(0, width - robot_w)
+            y = random.randint(0, height - robot_h)
+
+        window.fill((0, 0, 0))
+        window.blit(robot, (x, y))
+        pygame.display.flip()
+
+        if event.type == pygame.QUIT:
+            exit()
